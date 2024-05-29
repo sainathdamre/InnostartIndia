ec7202a740c23bac7934df3bec3a4f8de83ddf36
sumitsantoshkekan <sumitkekan@gmailcom>
2024-05-29 08:40:40 +0530

    hii

--- server/api/routes/auth.route.js
+++ server/api/routes/auth.route.js
@@ -5,8 +5,8 @@ import { verifyToken } from '../utils/verifyUser.js';
 const router = express.Router();
 
 router.post('/signup', signUp);
-router.post('/login', verifyToken, signIn);
-router.post('/send-connection-request', verifyToken, sendConnectionRequest);
+router.post('/login', signIn);
+router.post('/send-connection-request', sendConnectionRequest);
 router.post('/accept-connection-request', verifyToken, acceptConnectionRequest);
 router.get('/pending-connection-requests', verifyToken, getPendingConnectionRequests);
 router.get('/users', getAllUsers);
